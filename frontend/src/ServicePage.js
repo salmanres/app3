9a99f37518d615835bb2be241cac7df04616332c
salman_siddeeque <salmanres@gmail.com>
2024-09-03 14:54:42 +0530

    Salman

--- frontend/src/ServicePage.js
+++ frontend/src/ServicePage.js
@@ -1,6 +1,6 @@
-// export const backendurl = "https://app3-2f71.onrender.com";
+export const backendurl = "https://app3-2f71.onrender.com";
 
-export const backendurl = "http://localhost:3500";
+// export const backendurl = "http://localhost:3500";
 
 export const razorpay_key_id = "rzp_test_vDzu7TD9fGJIwW";
 export const razorpay_key_secret = "5mgkusn2iJNWlE7tGndORCrw";
